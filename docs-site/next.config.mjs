dfb9725c2a2a8be2b6c8e9e0915a85b2dbb4647a
Sagi Shabtai <sagi.shabtai@outlook.co.il>
2026-08-03 09:56:49 +0300

    Configure Nextra static documentation build

---
+++ docs-site/next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  contentDirBasePath: '/docs'
+})
+
+export default withNextra({
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+})
